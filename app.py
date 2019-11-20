884b8e6034dd324a54603dfab349bef903aaf7d1
az <18291072898@163.com>
2019-11-21 00:06:55 +0800

    add img

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
+
+from typing import List
+
 from flask import Flask, render_template
 
+from models import Photo, User, db
+
 app = Flask(__name__)
+# 内存 sqlite:///:memory: (or, sqlite://)
+# 相对路径 sqlite:///relative/path/to/file.db
+# 绝对路径 sqlite:////absolute/path/to/file.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db.init_app(app)
 
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    db.create_all()
+    # user = User("张三","qqq@qq.com")
+    # db.session.add(user)
+    # db.session.commit()
+    user: User = User.query.first()
+    import os
+    # filenamelist = list(os.walk("static/img"))
+    # for name in filenamelist[0][2]:
+    #     photo = Photo(os.path.join(os.path.abspath(filenamelist[0][0]), name), "QQ")
+    #     db.session.add(photo)
+    # db.session.commit()
+    photo_list: List[Photo] = Photo.query.filter_by(label="QQ").all()
+    photo_list = [{
+        "src": "/".join(img.photo_name.split(os.path.sep)[-3:]),
+        "alt": img.photo_name.split(os.path.sep)[-1],
+    } for img in photo_list]
+    return render_template('index.html', title=user.username, img=photo_list)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ models/__init__.py
@@ -0,0 +1,28 @@
+"""数据库模型"""
+
+import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Photo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    photo_name = db.Column(db.String(255), unique=True)
+    label = db.Column(db.String(80), index=True)
+
+    def __init__(self, photo_name, label):
+        self.photo_name = photo_name
+        self.label = label
